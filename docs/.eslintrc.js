24419f9c18eb59844daee1c839207484f067c3dc
Peter Brinkmann <peter.brinkmann@gmail.com>
2023-01-11 22:23:57 -0500

    .

---
+++ docs/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'max-len': [
+            'error',
+            {
+              'code': 80
+            }
+        ],
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
